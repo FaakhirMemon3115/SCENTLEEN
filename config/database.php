8d2c182dedb231cc187a9332f6a6d120b0c7f964
FaakhirMemon030 <alexwatson031105@gmail.com>
2026-06-25 16:33:18 +0500

    feat: add database configuration and PDO connection setup

--- config/database.php
+++ config/database.php
@@ -2,15 +2,15 @@
 // config/database.php
 
 $host = 'localhost';
-$db_name = 'SCENTLEEN_DB';
-$username = 'SCENTLEEN_DB';
-$password = 'Scentleen_DB1';
+$db_name = 'scentleen';
+$username = 'root';
+$password = '';
 
 try {
     $pdo = new PDO("mysql:host={$host};dbname={$db_name};charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch(PDOException $exception) {
+} catch (PDOException $exception) {
     echo "Connection error: " . $exception->getMessage();
     exit;
 }
